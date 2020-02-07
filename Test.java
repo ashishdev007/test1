101aaa97677702c6ae6a2d562b67d0b5c6abb91f
Shrill  Shrestha <shrest5@warhawks.ulm.edu>
2020-02-07 13:39:46 -0600

    Add printer feature

--- Test.java
+++ Test.java
@@ -1,6 +1,11 @@
 public class Test{
    public static void main(String[] args){
       System.out.println("Hello World");
+      printer("Hello World and universe");
+   }
+   
+   public static void printer(String phrase){
+      System.out.println(phrase);
    }
 
 }
