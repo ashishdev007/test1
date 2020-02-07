ac32bd9ae02aa465ea7b92bcd1ac07570f1c518e
Shrill  Shrestha <shrest5@warhawks.ulm.edu>
2020-02-07 13:54:44 -0600

    Add divider function

--- Test.java
+++ Test.java
@@ -1,11 +1,16 @@
 public class Test{
    public static void main(String[] args){
       System.out.println("Hello World");
-      printer("Hello World and universe");
+      
+      System.out.println(divider(10, 3));
    }
    
    public static void printer(String phrase){
       System.out.println(phrase);
+   }
+   
+   public static float divider(int no1, int no2){
+      return no1/no2;
    }
 
 }
